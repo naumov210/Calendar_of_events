2a18f4fa10c0afaab2d9d2fa5933955ef593711a
Andrey Naumov <naumov210@gmail.com>
2019-06-17 14:21:55 +0300

    Second commit

--- js/event.js
+++ js/event.js
@@ -7,3 +7,35 @@ console.log(may[0].firstElementChild.firstChild.firstElementChild);
 console.log(jun[0].firstElementChild.firstChild.firstElementChild);
 console.log(julay[0].firstElementChild.firstChild.firstElementChild);
 
+
+
+// let now = new Date();
+// let year = now.getFullYear();
+
+// let monthPast = now.getMonth();
+// let monthCurrent = now.getMonth()+1;
+// let monthFuture = now.getMonth()+2;
+
+// let tougle = 2;
+
+// function func(valueOfDate) {
+//     console.log(valueOfDate);
+// }
+
+
+// if(tougle == 0) {
+//     valueOfDate = monthPast;
+//     func(valueOfDate);
+//     // expect 5
+// }
+// else if(tougle == 1) {
+//     valueOfDate = monthCurrent;
+//     func(valueOfDate);
+//     // expect 6
+// }
+// else if(tougle == 2) {
+//     valueOfDate = monthFuture;
+//     func(valueOfDate);
+//     // expect 7
+// }
+
